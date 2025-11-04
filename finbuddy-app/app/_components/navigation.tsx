d863cff0f5efdf87c142dd024ed059b5bb030e0a
epezelj <emma.pezelj@gmail.com>
2025-11-04 13:54:28 +0100

    defined routes for template pages

---
+++ finbuddy-app/app/_components/navigation.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Page = {
+  title: string;
+  path: `/${string}`;
+};
+
+
+const pages: Page[] = [
+  { title: "Home", path: "/" },
+  {
+    title: "Transactions",
+    path: "/transactions",
+  },
+
+  {
+    title: "Accounts",
+    path: "/accounts",
+  },
+
+  {
+    title: "Reports",
+    path: "/reports",
+  },
+ 
+  {
+    title: "Categories",
+    path: "/categories",
+  },
+
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+
+  {
+    title: "Settings",
+    path: "/settings",
+  },
+];
+
+
+function processPage(page: Page, index: number, currentPath?: string) {
+  return (
+    <li key={index}>
+      <Link
+        href={page.path}
+        className={currentPath === page.path ? "font-extrabold" : ""}
+      >
+        {page.title}
+      </Link>
+    </li>
+  );
+}
+
+export function Navigation() {
+  const currentPath = usePathname();
+  return (
+    <nav>
+      <ul className="flex space-x-10 mb-5">
+        {pages.map((page, index) => processPage(page, index, currentPath))}
+      </ul>
+    </nav>
+  );
+}
