9010e8dccaa34fa7c5790b197f848c91cc2cb14f
epezelj <emma.pezelj@gmail.com>
2025-11-08 11:08:02 +0100

    Assignment3 routes

--- finbuddy-app/app/page.tsx
+++ finbuddy-app/app/page.tsx
@@ -1,40 +1,10 @@
-import { redirect } from "next/navigation";
-import { getSession, login, logout } from "../lib";
-
 import { Navigation } from "@components/navigation";
 
-
-
 export default async function Page() {
-  const session = await getSession();
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <Navigation />
       <h1 className="text-6xl font-extrabold tracking-tight">Login</h1>
-    <section>
-      <form
-        action={async (formData) => {
-          "use server";
-          await login(formData);
-          redirect("/");
-        }}
-      >
-        <input type="email" placeholder="Email" />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-      <form
-        action={async () => {
-          "use server";
-          await logout();
-          redirect("/");
-        }}
-      >
-        <button type="submit">Logout</button>
-      </form>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-    </section>
-
     </main>
   );
 }
