87aa7c044654d42432619b4ab30bafeda8367dd2
epezelj <emma.pezelj@gmail.com>
2025-12-02 21:35:23 +0100

    solving auth problems-2

--- finbuddy-app/prisma.config.ts
+++ finbuddy-app/prisma.config.ts
@@ -1,18 +1,3 @@
-// import { defineConfig, env } from "prisma/config";
-
-// export default defineConfig({
-//   schema: "prisma/schema.prisma",
-//   migrations: {
-//     path: "prisma/migrations",
-//   },
-//   engine: "classic",
-//   datasource: {
-//     url: env("DATABASE_URL"),
-//     //directUrl: env("DIRECT_URL")
-//   },
-// });
-
-
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
@@ -20,4 +5,11 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+    //directUrl: env("DIRECT_URL")
+  },
 });
+
+
